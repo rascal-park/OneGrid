82757e93428e64484689e72d3d7869c9d87109e7
jh.park <wogud772@gmail.com>
2025-11-10 23:30:45 +0900

    [251110] [박재형] - gitPage 404방지용 HashRouter 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
+// App.tsx
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import DocsLayout from './layout/DocsLayout';
 import BasicGridPage from './pages/BasicGridPage';
 
 const App: React.FC = () => {
 	return (
-		<BrowserRouter basename={import.meta.env.BASE_URL}>
+		<HashRouter /* basename 안 써도 됨 */>
 			<Routes>
-				{/* 공통 레이아웃 */}
 				<Route element={<DocsLayout />}>
-					{/* /OneGrid/ (== basename 뒤의 "/")로 들어왔을 때 */}
+					{/* #/ */}
 					<Route index element={<Navigate to="basic" replace />} />
-
-					{/* /OneGrid/basic */}
+					{/* #/basic */}
 					<Route path="basic" element={<BasicGridPage />} />
-
-					{/* 앞으로 확장 예:
-              <Route path="selection" element={<SelectionPage />} />
-              <Route path="sort" element={<SortDemoPage />} />
-          */}
 				</Route>
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	);
 };
 
